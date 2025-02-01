27a1c47c5e1a1a9d76df48ced3960e44237719d1
Ilham <mi1797128@gmail.com>
2025-02-01 15:39:53 +0700

    feat: slicing onboarding

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -31,7 +31,87 @@ class OnboardingPage extends StatelessWidget {
                 options: CarouselOptions(
                     height: 331,
                     enableInfiniteScroll: false,
-                    viewportFraction: 1))
+                    viewportFraction: 1)),
+            const SizedBox(
+              height: 80,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 24),
+              decoration: BoxDecoration(
+                  color: whiteColor, borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                children: [
+                  Text(
+                    'Grow Your\nFinancial Today',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    'Our system is helping you to\nachieve a better goal',
+                    style: greyTextStyle.copyWith(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 12,
+                        width: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          color: blueColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Container(
+                        height: 12,
+                        width: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          color: lightBackgroundColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Container(
+                        height: 12,
+                        width: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          color: lightBackgroundColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        height: 50,
+                        width: 150,
+                        child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                                backgroundColor: purpleColor,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(56))),
+                            child: Text(
+                              'Continue',
+                              style: whiteTextStyle.copyWith(
+                                  fontSize: 16, fontWeight: semiBold),
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
