682b20bd67e907a8dc87ee16350755ca742b0bce
Ilham <mi1797128@gmail.com>
2025-02-02 20:12:24 +0700

    feat: slicing home page - bottom navigation bar

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,6 +8,44 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showUnselectedLabels: true,
+          unselectedLabelStyle:
+              blackTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+          showSelectedLabels: true,
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColor,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_history.png',
+                width: 20,
+              ),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_statistic.png',
+                width: 20,
+              ),
+              label: 'Statistic',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_reward.png',
+                width: 20,
+              ),
+              label: 'Reward',
+            ),
+          ]),
     );
   }
 }
