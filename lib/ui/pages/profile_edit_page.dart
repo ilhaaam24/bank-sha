2d6ce1e0337325cc5222abfaf5f2e3855e358b05
Ilham <mi1797128@gmail.com>
2025-02-03 22:00:56 +0700

    feat : navigation with result

---
+++ lib/ui/pages/profile_edit_page.dart
@@ -0,0 +1,15 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class ProfileEditPage extends StatelessWidget {
+  const ProfileEditPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      appBar: 
+      AppBar(title: Text(''),),
+    );
+  }
+}
