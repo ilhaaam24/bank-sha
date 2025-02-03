d50b2479e631ab3803112af26681dd3f4c15c605
Ilham <mi1797128@gmail.com>
2025-02-03 13:52:54 +0700

    feat: slicing profile page

---
+++ lib/ui/pages/profile_page.dart
@@ -0,0 +1,105 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widget/buttons.dart';
+import 'package:bank_sha/ui/widget/profile_menu_item.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: lightBackgroundColor,
+        iconTheme: IconThemeData(color: blackColor),
+        centerTitle: true,
+        title: Text(
+          'My Profile',
+          style: blackTextStyle.copyWith(fontSize: 20, fontWeight: semiBold),
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 30),
+            padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 30),
+            decoration: BoxDecoration(
+                color: whiteColor, borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              children: [
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage('assets/img_profile.png'))),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                        height: 28,
+                        width: 28,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: whiteColor,
+                        ),
+                        child: Center(
+                            child: Icon(Icons.check_circle,
+                                color: greenColor, size: 24))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Shayna Hanna',
+                  style:
+                      blackTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_edit_profile.png',
+                  title: 'Edit Profile',
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_pin.png',
+                  title: 'My PIN',
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_wallet.png',
+                  title: 'Wallet Settings',
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_my_rewards.png',
+                  title: 'My Rewards',
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_help.png',
+                  title: 'Help Center',
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  iconUrl: 'assets/ic_logout.png',
+                  title: 'Log Out',
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 87,
+          ),
+          CustomTextButton(title: 'Report a Problem', onPressed: () {}),
+        ],
+      ),
+    );
+  }
+}
