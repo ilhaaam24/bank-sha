20b55a936c66b07dd5180fd805e3aac540356201
Ilham <mi1797128@gmail.com>
2025-02-22 20:13:40 +0700

    feat : change pin to dynamic

--- lib/ui/pages/pin_page.dart
+++ lib/ui/pages/pin_page.dart
@@ -1,7 +1,9 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
 import 'package:bank_sha/shared/shared_methods.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widget/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PinPage extends StatefulWidget {
   const PinPage({super.key});
@@ -12,6 +14,8 @@ class PinPage extends StatefulWidget {
 
 class _PinPageState extends State<PinPage> {
   final TextEditingController pinController = TextEditingController();
+  String pin = '';
+  bool isError = false;
 
   addPin(String number) {
     if (pinController.text.length < 6) {
@@ -20,9 +24,10 @@ class _PinPageState extends State<PinPage> {
       });
     }
     if (pinController.text.length == 6) {
-      if (pinController.text == '123123') {
+      if (pinController.text == pin) {
         Navigator.pop(context, true);
       } else {
+        isError = true;
         showCustomSnackbar(
             context, 'PIN yang anda masukan salah, Silahkan coba lagi');
         setState(() {
@@ -35,9 +40,19 @@ class _PinPageState extends State<PinPage> {
   deletePin() {
     if (pinController.text.isNotEmpty) {
       setState(() {
+        isError = false;
         pinController.text =
             pinController.text.substring(0, pinController.text.length - 1);
       });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final authState = context.read<AuthBloc>().state;
+    if (authState is AuthSuccess) {
+      pin = authState.user.pin!;
     }
   }
 
@@ -66,7 +81,10 @@ class _PinPageState extends State<PinPage> {
                   controller: pinController,
                   obscuringCharacter: '*',
                   style: whiteTextStyle.copyWith(
-                      fontSize: 36, fontWeight: medium, letterSpacing: 16),
+                      fontSize: 36,
+                      fontWeight: medium,
+                      letterSpacing: 16,
+                      color: isError ? redColor : whiteColor),
                   obscureText: true,
                   showCursor: false,
                   decoration: InputDecoration(
