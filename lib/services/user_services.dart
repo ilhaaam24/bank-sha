915b198c5ae34e960079c41b1b63741993e7c46b
Ilham <mi1797128@gmail.com>
2025-03-03 06:53:10 +0700

    feat:  service get user

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:bank_sha/models/user_edit_form_model.dart';
+import 'package:bank_sha/models/user_model.dart';
 import 'package:bank_sha/services/auth_services.dart';
 import 'package:bank_sha/shared/shared_values.dart';
 import 'package:http/http.dart' as http;
@@ -14,6 +15,43 @@ class UserService {
       if (res.statusCode != 200) {
         throw jsonDecode(res.body)['message'];
       }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<UserModel>> getRecentUser() async {
+    try {
+      final token = await AuthServices().getToken();
+      final res =
+          await http.get(Uri.parse('$baseUrl/transfer_histories'), headers: {
+        'Authorization': token,
+      });
+
+      if (res.statusCode == 200) {
+        return List<UserModel>.from(jsonDecode(res.body)['data']
+            .map((user) => UserModel.fromJson(user))).toList();
+      }
+      throw jsonDecode(res.body)['message'];
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<UserModel>> getUserByUsername(String username) async {
+    try {
+      final token = await AuthServices().getToken();
+      final res =
+          await http.get(Uri.parse('$baseUrl/users/$username'), headers: {
+        'Authorization': token,
+      });
+
+      if (res.statusCode == 200) {
+        return List<UserModel>.from(
+                jsonDecode(res.body).map((user) => UserModel.fromJson(user)))
+            .toList();
+      }
+      throw jsonDecode(res.body)['message'];
     } catch (e) {
       rethrow;
     }
